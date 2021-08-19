459b12a16122b84764976b007724ab0a66a69bf7
tiagopapile <tiagopapile@gmail.com>
2021-08-19 11:13:23 -0300

    Adiciona opção de vídeos mais vistos a PAListVideosColumn

--- Blocks/PAListVideosColumn/views/frontend.blade.php
+++ Blocks/PAListVideosColumn/views/frontend.blade.php
@@ -1,32 +1,37 @@
 @if(is_admin())
 	<img class="img-preview" src="{{ get_stylesheet_directory_uri() }}/Blocks/PAListVideosColumn/preview.png"/>
 @else
-	
-	<div class="pa-widget pa-w-list-posts col-md-4">
-			
-		@notempty($items) 
-
+	@notempty($items) 
+		<div class="pa-widget pa-w-list-posts col-md-4 pa-w-list-videos mb-5">			
 			<h2 class="mb-4">{{ $title }}</h2>
 
 			@foreach ($items as $id)
-			<div class="card mb-2 mb-xl-4 border-0">
-				<a href="{{ get_the_permalink($id) }}" title="{{ get_the_title($id) }}"">
-					<div class="row">
-						<div class="col-6">
-							<div class="ratio ratio-16x9">
-								<figure class="figure m-xl-0">
-									<img src="{{ check_immg($id, 'full') }}" class="figure-img img-fluid rounded m-0" alt="{{ get_the_title($id) }}">
-								</figure>	
+				<div class="card mb-2 mb-xl-4 border-0">
+					<a href="{{ get_the_permalink($id) }}" title="{{ get_the_title($id) }}"">
+						<div class="row">
+							<div class="col-6">
+								<div class="ratio ratio-16x9">
+									<figure class="figure m-xl-0">
+										<img src="{{ check_immg($id, 'full') }}" class="figure-img img-fluid rounded m-0" alt="{{ get_the_title($id) }}">
+
+										@hasfield('video_length', $id)
+											<div class="figure-caption position-absolute w-100 h-100 d-block">
+												<span class="pa-video-time position-absolute px-2 rounded-1">
+													<i class="far fa-clock me-1" aria-hidden="true"></i> @videolength($id)
+												</span>
+											</div>
+										@endfield
+									</figure>	
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="card-body p-0">
+									<h3 class="card-title h6">{{ get_the_title($id) }}</h3>
+								</div>
 							</div>
 						</div>
-						<div class="col-6">
-							<div class="card-body p-0">
-								<h3 class="card-title h6">{{ get_the_title($id) }}</h3>
-							</div>
-						</div>
-					</div>
-				</a>
-			</div>
+					</a>
+				</div>
 			@endforeach
 
 			@notempty($enable_link)
@@ -38,12 +43,6 @@
 					{!! $link['title'] !!}
 				</a>
 			@endnotempty
-
-		@else
-
-			<p>Não foi encontrado nenhum post para esta seção.</p>
-
-		@endisset
-		
-	</div>
+		</div>
+	@endnotempty
 @endif
