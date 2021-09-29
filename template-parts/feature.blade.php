94ee562a087a3cbf5f4ad765e4cd1fb7f8d8af95
Tiago Papile <tiagopapile@gmail.com>
2021-09-29 14:45:36 -0300

    Ajusta destaque de archive

--- template-parts/feature.blade.php
+++ template-parts/feature.blade.php
@@ -8,9 +8,13 @@
                     <figure class="figure m-xl-0 w-100">
                         <img src="{{ check_immg($post->ID, 'full') }}" class="figure-img img-fluid m-0 rounded w-100 h-100 object-cover" alt="{{ get_the_title($post->ID) }}">
 
-                        <div class="figure-caption position-absolute w-100 h-100 d-block">
-                            <i class="pa-play far fa-play-circle position-absolute" aria-hidden="true"></i>
-                        </div>
+                        @hasfield('video_length', $post->ID)
+                            <div class="figure-caption position-absolute w-100 h-100 d-block">
+                                <span class="pa-video-time position-absolute px-2 rounded-1">
+                                    <i class="far fa-clock me-1" aria-hidden="true"></i> @videolength($post->ID)
+                                </span>
+                            </div>
+                        @endfield
                     </figure>
                 </div>
             </a>
